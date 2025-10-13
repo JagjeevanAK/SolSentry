64a0983bdd46d9fb84b9b603f18796f34db90e4e
Jagjeevan Kashid <jagjeevandev97@gmail.com>
2025-10-14 03:58:56 +0530

    minor changes
    Signed-off-by: Jagjeevan Kashid <jagjeevandev97@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-import { runWorkflow } from "./workflow/graph";
-
-export { runWorkflow };
-
-export * from "./utils/helius-api";
-export * from "./utils/solscan-api";
-export * from "./tools/transaction-tools";
-export * from "./types";
-export * from "./jobs";
+import { runWorkflow, StateAnnotation } from "./workflow/graph";
 
 export async function analyzeQuery(userQuery: string): Promise<string> {
     return await runWorkflow(userQuery);
 }
+
+export type { WorkflowState } from "./types";
+export { StateAnnotation };
 
